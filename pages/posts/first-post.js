0e7c971f9655cffab0a72a6a2743e7822fe41945
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2022-07-21 18:06:15 +0800

    Assets metadata and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
-  );
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <Image
+                src="/images/profile.jpg"
+                height={144}
+                width={144}
+                alt="Your Name"></Image>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    );
 }
